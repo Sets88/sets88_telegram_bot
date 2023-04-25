819ac37c792586a09d7100f9960fa9ebc6849ea2
Maxim Nikitenko <mnikitenko@expertoption.com>
2023-04-25 19:23:34 +0300

    add option to set additional params in replicate models

--- replicate_module.py
+++ replicate_module.py
@@ -1,7 +1,9 @@
 import os
 import asyncio
 import functools
+import json
 from io import BytesIO
+from copy import copy
 
 from telebot.types import Message
 import replicate
@@ -27,13 +29,51 @@ REPLICATE_MODELS = {
         'description': 'Real-ESRGAN is a GAN-based image super-resolution model trained on real-world images. It can be used to upscale images to 4x the original resolution.',
         'replicate_id': 'nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b',
         'input_type': 'photo',
-        'output_type': 'file'
+        'output_type': 'file',
+        'available_params': {
+            'scale': {
+                'type': 'int',
+                'default': 4,
+                'min': 2,
+                'max': 10,
+                'description': 'Scale factor'
+            },
+        }
     },
     'kandinsky': {
         'description': 'Kandinsky 2.1, text2img model trained on LAION HighRes and fine-tuned on internal datasets',
         'replicate_id': 'ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f',
         'input_type': 'text',
-        'output_type': 'photo'
+        'output_type': 'photo',
+        'available_params': {
+            'num_inference_steps': {
+                'type': 'int',
+                'default': 50,
+                'min': 1,
+                'max': 1000,
+                'description': 'Number of denoising steps'
+            },
+            'guidance_scale': {
+                'type': 'int',
+                'default': 4,
+                'description': 'Scale for classifier-free guidance'
+            },
+            'scheduler': {
+                'type': 'select',
+                'options': ['ddim_sampler', 'p_sampler', 'plms_sampler'],
+                'default': 'p_sampler'
+            },
+            'width': {
+                'type': 'int',
+                'default': 512,
+                'description': 'Width of the image'
+            },
+            'height': {
+                'type': 'int',
+                'default': 512,
+                'description': 'Height of the image'
+            },
+        }
     },
     'dolly': {
         'description': 'Dolly, an open source instruction-tuned large language model developed by Databricks',
@@ -45,7 +85,18 @@ REPLICATE_MODELS = {
         'description': 'Blip, bootstrapping Language-Image Pre-training, send photo to get caption',
         'replicate_id': 'salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746',
         'input_type': 'photo',
-        'output_type': 'text'
+        'output_type': 'text',
+        'available_params': {
+            'task': {
+                'type': 'select',
+                'options': ['image_captioning', 'visual_question_answering'],
+                'default': 'image_captioning'
+            },
+            'question': {
+                'type': 'str',
+                'description': 'Question for VQA'
+            },
+        }
     },
     'openjourney': {
         'description': 'OpenJourney, Stable Diffusion fine tuned on Midjourney v4 images.',
@@ -56,7 +107,8 @@ REPLICATE_MODELS = {
 }
 
 
-def replicate_execute(replicate_id, input_data):
+def replicate_execute(replicate_id: str, input_data: dict):
+    logger.info(json.dumps(input_data))
     output = replicate.run(
         replicate_id,
         input=input_data
@@ -64,16 +116,123 @@ def replicate_execute(replicate_id, input_data):
     return output
 
 
-async def replicate_choose_model(model: str, botnav: TeleBotNav, message: Message) -> None:
-    if model not in REPLICATE_MODELS:
+async def replicate_set_select_param(param_name: str, value: str, botnav: TeleBotNav, message: Message):
+    await botnav.bot.delete_message(message.chat.id, message.message_id)
+    message.state_data['replicate_params'][param_name] = value
+    await botnav.bot.send_message(message.chat.id, f"Param {param_name} was set to: {value}")
+    await replicate_print_params_buttons(botnav, message)
+
+
+async def replicate_set_input_param(param_name: str, botnav: TeleBotNav, message: Message):
+    param = REPLICATE_MODELS[message.state_data['replicate_model']]['available_params'][param_name]
+    value = message.text
+
+    if param['type'] == 'int':
+        value = int(value)
+
+    message.state_data['replicate_params'][param_name] = value
+    await botnav.bot.send_message(message.chat.id, f"Param {param_name} was set to: {value}")
+    await replicate_print_params_buttons(botnav, message)
+
+
+async def replicate_choose_param(model_name_param_name: str, botnav: TeleBotNav, message: Message):
+    model_name, param_name = model_name_param_name.split(':')
+
+    if model_name not in REPLICATE_MODELS:
         return
 
-    message.state_data['replicate_model'] = model
+    model = REPLICATE_MODELS[model_name]
 
-    await botnav.bot.send_message(message.chat.id, "Model was set to: " + REPLICATE_MODELS[model]['description'])
+    if param_name not in model['available_params']:
+        return
+
+    param = model['available_params'][param_name]
+    await botnav.bot.delete_message(message.chat.id, message.message_id)
+
+    if param['type'] == 'int':
+        text = "Please enter integer value "
+        if param.get('description'):
+            text += f"({param['description']}) "
+        if param.get('min'):
+            text += f"greater than {param['min']} "
+        if param.get('max'):
+            text += f"less than {param['max']} "
+        if param.get('default'):
+            text += f"or leave empty for default value ({param['default']})"
+
+        await botnav.set_next_handler(message, functools.partial(replicate_set_input_param, param_name))
+        await botnav.bot.send_message(message.chat.id, text)
+        return
+
+    if param['type'] == 'str':
+        text = "Please enter string value "
+        if param.get('description'):
+            text += f"({param['description']}) "
+        if param.get('default'):
+            text += f"or leave empty for default value ({param['default']})"
+
+        await botnav.set_next_handler(message, functools.partial(replicate_set_input_param, param_name))
+        await botnav.bot.send_message(message.chat.id, text)
+        return
+
+    if param['type'] == 'select':
+        text = "Please choose one of the following options "
+        if param.get('description'):
+            text += f"({param['description']}) "
+
+        buttons = {
+            x: functools.partial(replicate_set_select_param, param_name, x) for x in param['options']
+        }
+
+        await botnav.print_buttons(
+            message.chat.id,
+            buttons,
+            text=text,
+            row_width=2,
+        )
 
 
-async def download_file(url):
+def replicate_get_params_buttons(model_name: str):
+    model = REPLICATE_MODELS[model_name]
+    buttons = {
+        x: functools.partial(replicate_choose_param, f'{model_name}:{x}') for x in model['available_params'].keys()
+    }
+
+    return buttons
+
+
+async def replicate_print_params_buttons(botnav: TeleBotNav, message: Message):
+    model = REPLICATE_MODELS[message.state_data['replicate_model']]
+
+    if model.get('available_params'):
+        buttons = replicate_get_params_buttons(message.state_data['replicate_model'])
+
+        await botnav.print_buttons(
+            message.chat.id,
+            buttons,
+            text="If you want to set additional params use buttons:",
+            row_width=2,
+        )
+
+
+async def replicate_choose_model(model_name: str, botnav: TeleBotNav, message: Message) -> None:
+    if model_name not in REPLICATE_MODELS:
+        return
+
+    model = REPLICATE_MODELS[model_name]
+
+    default_params = copy(model.get('default_params', {}))
+
+    message.state_data['replicate_model'] = model_name
+    message.state_data['replicate_params'] = default_params
+
+    await botnav.bot.send_message(message.chat.id, "Model was set to: " + REPLICATE_MODELS[model_name]['description'])
+
+    if model.get('available_params'):
+        await replicate_print_params_buttons(botnav, message)
+
+
+async def download_file(url: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status != 200:
@@ -107,9 +266,8 @@ async def replicate_message_handler(botnav: TeleBotNav, message: Message) -> Non
         return
 
     replicate_model = REPLICATE_MODELS[replicate_model_name]
-    input_data = {}
 
-    input_data.update(replicate_model.get('default_params', {}))
+    input_data = message.state_data.get('replicate_params', {})
 
     if message.content_type == 'text' != replicate_model['input_type']:
         return
@@ -139,6 +297,7 @@ async def replicate_message_handler(botnav: TeleBotNav, message: Message) -> Non
                     await botnav.bot.send_photo(message.chat.id, photo)
             elif isinstance(result, str):
                 await botnav.bot.send_photo(message.chat.id, result)
+
         if replicate_model['output_type'] == 'text':
             parts = []
             for part in result:
@@ -149,6 +308,7 @@ async def replicate_message_handler(botnav: TeleBotNav, message: Message) -> Non
                     await botnav.bot.send_message(message.chat.id, "".join(parts))
                     parts = []
             await botnav.bot.send_message(message.chat.id, "".join(parts))
+
         if replicate_model['output_type'] == 'file':
             if isinstance(result, list):
                 for document_url in result:
