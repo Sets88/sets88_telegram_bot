8b6519d175a090db9df75478b5e38bbb6409a0d4
alexue4 <admin@alexue4.ru>
2024-06-20 19:59:16 +0300

    Add new claude3.5 model

--- claude_module.py
+++ claude_module.py
@@ -10,6 +10,7 @@ from telebot_nav import TeleBotNav
 
 
 CLAUDE_MODELS = [
+    'claude-3-5-sonnet-20240620',
     'claude-3-haiku-20240307',
     'claude-3-sonnet-20240229',
     'claude-3-opus-20240229'
