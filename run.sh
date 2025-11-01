676ad9eb8867a3daa9f715f71f2c1fa36169e030
Maksim Nikitenko <iam@sets88.com>
2025-11-01 09:44:07 +0200

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 touch log.txt
-chmod 666 log.txt
 docker stop gptchat_bot
 docker rm gptchat_bot
 docker build -t gptchat_bot .
@@ -9,7 +8,11 @@ docker run -d \
     -v $(pwd)/conv:/app/conv \
     -v $(pwd)/schedules:/app/schedules \
     -v $(pwd)/uploads:/app/uploads \
-    -v $(pwd)/memort:/app/memory \
+    -v $(pwd)/memory:/app/memory \
     -v $(pwd)/config.json:/app/config.json \
     -v $(pwd)/log.txt:/app/log.txt \
     gptchat_bot
+docker exec -u 0 gptchat_bot chown 1000:1000 /app/memory
+docker exec -u 0 gptchat_bot chown 1000:1000 /app/uploads
+docker exec -u 0 gptchat_bot chown 1000:1000 /app/log.txt
+docker exec -u 0 gptchat_bot chown 1000:1000 /app/schedules
